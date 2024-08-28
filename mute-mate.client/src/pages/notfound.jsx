e7ddae8b40f383f23ae15ecd7134083efc17e881
Dion <146066755+urie8@users.noreply.github.com>
2024-08-28 17:09:55 +0200

    Delete mute-mate.client directory

--- mute-mate.client/src/pages/notfound.jsx
+++
@@ -1,9 +0,0 @@
-function notFound() {
-  return (
-    <>
-      <div>Not found</div>
-    </>
-  );
-}
-
-export default notFound;
--- mute-mate.client/src/pages/practise.jsx
+++
@@ -1,5 +0,0 @@
-function practise() {
-  return <></>;
-}
-
-export default practise;
