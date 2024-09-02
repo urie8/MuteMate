616c5dc7cbd210f507ebea0c41c5a42cbd6f835b
PaulineI <pauline.ibraim@gmail.com>
2024-09-02 11:57:10 +0200

    UpdateProfile

--- MuteMate.Client/src/pages/myPage.jsx
+++ MuteMate.Client/src/pages/myPage.jsx
@@ -3,6 +3,9 @@ import "../Styles/myPage.css";
 import banana from "../images/banan1.png";
 import vines from "../images/vines.png";
 import { useNavigate } from "react-router-dom";
+import colorwheel from "/public/MiiaImages/color-wheel.png";
+import alphabet from "/public/MiiaImages/abc.png";
+import livestock from "/public/MiiaImages/livestock.png";
 
 //Import från API, användaruppgifter och poäng.
 
@@ -10,22 +13,51 @@ function myPage() {
   return (
     <>
       <div className="Container-profile">
-        <h1 className="title-profile">
-          PROFILE PAGE
-          <div className="picture-container">
+        <div className="title-profile-container">
+          <h1 className="title-profile">
+            {/* PROFILE PAGE */}
+            {/* <div className="picture-container">
             <img src={vines} alt="vines" className="vines-1" />
             <img src={vines} alt="vines" className="vines-1" />
+          </div> */}
+          </h1>
+          <div className="userNameContainer">
+            <div className="display-userName">username </div>
+            {/* Username ska hämtas här */}
           </div>
-        </h1>
+        </div>
+        <div className="quotes-container">Motiverande quotes här!</div>
+        {/*Container för category.
+        Container under för att se alla bananer /kategori */}
 
-        <div className="userNameContainer">
-          <div className="display-userName">username </div>
-          {/* Username ska hämtas här */}
+        <div className="score-display">
+          <div className="img-container-profile">
+            <img
+              src={colorwheel}
+              alt="colorwheel"
+              className="colorwheel-profile"
+            />
+            <div className="banana-points-container">
+              <img src={banana} alt="banan" className="banana2" /> = 15
+            </div>
+          </div>
+          <div className="img-container-profile">
+            <img src={alphabet} alt="alphabet" className="alphabet-profile" />
+            <div className="banana-points-container">
+              <img src={banana} alt="banan" className="banana2" /> = 12
+            </div>
+          </div>
+          <div className="img-container-profile">
+            <img src={livestock} alt="animals" className="animals-profile" />
+            <div className="banana-points-container">
+              <img src={banana} alt="banan" className="banana2" /> = 17
+            </div>
+          </div>
         </div>
         <div className="banana-display">
           <img src={banana} alt="banan" className="banana1" />
-          <div className="banana-point">Hämta bananer här från API</div>
-          <div className="banana-text">Your collected bananas!</div>
+          {/* <div className="banana-point">Hämta bananer här från API</div> */}
+          <div className="banana-text">Total bananas!</div>
         </div>
       </div>
     </>
