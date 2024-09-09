f63e8ec11c802dc08bdbf9b99a00669426dd4d83
miiamole <miia_pelaez@hotmail.com>
2024-09-09 11:42:49 +0200

    bilderna flippar på sidorna där man väljer categori

--- MuteMate.Client/src/pages/categoryQuiz.jsx
+++ MuteMate.Client/src/pages/categoryQuiz.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import CategoryPractiseQuiz from "../components/CategoryPractiseQuiz";
 function categoryQuiz() {
   return (
