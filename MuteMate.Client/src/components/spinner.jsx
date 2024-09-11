456682d2eb355e018a6be651cdd4a0689026a983
miiamole <miia_pelaez@hotmail.com>
2024-09-11 20:37:37 +0200

    andra försöket

--- MuteMate.Client/src/components/spinner.jsx
+++ MuteMate.Client/src/components/spinner.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../Styles/spinner.css";
 import angryMonkey from "../images/angryMonkey.png";
+import banana from "../images/banana.png";
 
 
 function Spinner() {
@@ -9,15 +10,26 @@ function Spinner() {
       <div className="spinner-wrapper">
         <div className="spinner-container">
           <div className="spinner"></div>
-          <img
-            src={angryMonkey}
-            alt="Center"
-            className="spinner-monkey-img"
-          />
+          <img src={angryMonkey} alt="Monkey" className="spinner-monkey-img" />
+          <img class="spinner-banana-img banana-1" src={banana} alt="Banana" />
+          <img class="spinner-banana-img banana-2" src={banana} alt="Banana" />
+          <img class="spinner-banana-img banana-3" src={banana} alt="Banana" />
+          <img class="spinner-banana-img banana-4" src={banana} alt="Banana" />
+          <img class="spinner-banana-img banana-5" src={banana} alt="Banana" />
+          <img class="spinner-banana-img banana-6" src={banana} alt="Banana" />
+          <img class="spinner-banana-img banana-7" src={banana} alt="Banana" />
+          <img class="spinner-banana-img banana-8" src={banana} alt="Banana" />
+          <img class="spinner-banana-img banana-9" src={banana} alt="Banana" />
+          <img class="spinner-banana-img banana-10" src={banana} alt="Banana" />
         </div>
-      </div> 
+      </div>
 
-      
+      {/* <div className="spinner-wrapper">
+        <div className="spinner-container">
+          <div className="spinner"></div>
+          <img src={angryMonkey} alt="Monkey" className="spinner-monkey-img" />
+        </div>
+      </div> */}
     </>
   );
 }
