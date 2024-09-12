a48833e97a79fb4f0f01019f66f1474c72812728
miiamole <miia_pelaez@hotmail.com>
2024-09-12 09:59:07 +0200

    fix

--- MuteMate.Client/src/pages/test.jsx
+++ MuteMate.Client/src/pages/test.jsx
@@ -1,39 +1,34 @@
 import React, { useEffect, useState } from "react";
-
+import Result from "../components/result";
 import Spinner from "../components/spinner";
 import { ENDPOINTS } from "../api/apiEndpoints";
 
-
 function Test() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState(null);
 
-useEffect(() => {
-  
-  setTimeout(() => {
-    fetch(ENDPOINTS.GETCATEGORIESLETTERS)
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data.$values);
-        setIsLoading(false); // När datan är laddad, ändra tillståndet
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setIsLoading(false); // Ändra tillståndet även vid fel
-      });
-  }, 2000); // 2 sekunders fördröjning
-}, []);
-
-
-
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(ENDPOINTS.GETCATEGORIESLETTERS)
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data.$values);
+          setIsLoading(false); // När datan är laddad, ändra tillståndet
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+          setIsLoading(false); // Ändra tillståndet även vid fel
+        });
+    }, 2000); // 2 sekunders fördröjning
+  }, []);
 
   return (
     <>
+      <Result />
       {isLoading ? (
-        <Spinner /> // Visa spinner medan sidan laddas
+        <Spinner />
       ) : (
         <div>
-          {/* Visa innehåll när datan är hämtad */}
           <h1>Data är laddad!</h1>
           <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
