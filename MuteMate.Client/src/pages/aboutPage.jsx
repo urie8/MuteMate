915b916277a958494febe276da276db654ceca55
pelicanpubis <norah.binsalamah@hotmail.com>
2024-09-12 09:41:52 +0200

    aboutpage är nu respnsivt för både desktop och mobil

--- MuteMate.Client/src/pages/aboutPage.jsx
+++ MuteMate.Client/src/pages/aboutPage.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import "../Styles/aboutPage.css";
+import pawprint from "../images/pawprint.png";
+import colorpic from "../images/chromatic.png";
+import monkey from "../images/Monkeybubble.png";
+
+import abc2 from "../images/abc2.png";
+import { NavLink } from "react-router-dom";
 
 function AboutPage() {
   return (
@@ -11,17 +17,44 @@ function AboutPage() {
             <div className="intro-text">
               Welcome to our website! Here, kids can have fun while learning and
               practicing sign language through interactive quizzes.
-        
             </div>
+
             <div className="different-offers-text">
               We offer different types of sign language quizzes where kids can
               practice and test their knowledge on:
             </div>
+
             <div className="about-category-containers">
-              <div className="animals-letters-colors-text">Animals</div>
-              <div className="animals-letters-colors-text">Letters</div>
-              <div className="animals-letters-colors-text">Colors</div>
+              <NavLink to="/quiz/animals" className="about-text-links">
+                <div className="animals-letters-colors-text">
+                  Animals
+                  <img
+                    className="about-paw-image"
+                    src={pawprint}
+                    alt="Pawprint"
+                  />
+                </div>
+              </NavLink>
+              <NavLink to="/quiz/letters" className="about-text-links">
+                <div className="animals-letters-colors-text">
+                  Letters{" "}
+                  <img className="about-abc-image" src={abc2} alt="ABC" />
+                </div>
+              </NavLink>
+
+              <NavLink to="/quiz/colors" className="about-text-links">
+                <div className="animals-letters-colors-text">
+                  Colors
+                  <img
+                    className="about-color-image"
+                    src={colorpic}
+                    alt="Pawprint"
+                  />
+                </div>
+                {/* <img className="about-monkey-image" src={monkey} alt="ABC" /> */}
+              </NavLink>
             </div>
+
             <div className="ending-text">
               These quizzes are designed to make learning fun and help kids
               build their confidence in using sign language. Let’s start
